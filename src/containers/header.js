eb1a118b3e5330e70df8eeeee8fff5d00780b93b
Divesh2201 <diveshthakker2201@gmail.com>
2021-03-26 18:10:25 +0530

    fix cache

--- src/containers/header.js
+++ src/containers/header.js
@@ -3,11 +3,10 @@ import { Header, OptForm, Feature } from '../components'
 import * as ROUTES from '../constants/routes'
 
 export default function HeaderContainer({children}) {
-  let logo = 'https://divesh2201.github.io/netflix/images/icons/logo.svg'
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+        <Header.Logo to={ROUTES.HOME} src='https://divesh2201.github.io/netflix/images/icons/logo.svg' alt="Netflix" />
         <Header.ButtonLink to={ROUTES.SIGN_IN} >Sign In</Header.ButtonLink>
       </Header.Frame>
       <Feature>
